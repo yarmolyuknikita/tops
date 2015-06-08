8f1451985593f61f960c7fcb5b3b24c3878db66c
Никита Ярмолюк <nike993@mail.ru>
2015-06-09 02:04:28 +0600

    1

---
+++ ParseStarterProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ParseStarterProject
+//
+//  Created by Никита Ярмолюк on 6/5/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
